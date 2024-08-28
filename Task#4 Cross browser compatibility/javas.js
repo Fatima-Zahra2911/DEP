e4a83557a4a10800fc7ddbeb3244f6ac5700f4ed
Fatima-Zahra2911 <fatimaakmal2911@gmail.com>
2024-08-29 00:49:23 +0500

    Add files via upload

---
+++ Task#4 Cross browser compatibility/javas.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerMenu = document.querySelector(".hamburger-menu");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburgerMenu.addEventListener("click", function () {
+    navLinks.classList.toggle("nav-active");
+  });
+});
+const acc = document.getElementsByClassName("accordion");
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+
+    const panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
